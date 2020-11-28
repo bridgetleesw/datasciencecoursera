607495d12c3f9983bb484f1e0109bc7e4e4ea3f6
bridgetleesw <58164307+bridgetleesw@users.noreply.github.com>
2020-11-28 12:32:08 +0800

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,16 +1,16 @@
 ### QN 1 ####
 features <- read.table("~/features.txt", quote="\"", comment.char="")
-subject_train <- read.table("~/subject_train.txt", quote="\"", comment.char="")
-X_train <- read.table("~/X_train.txt", quote="\"", comment.char="")
-y_train <- read.table("~/y_train.txt", quote="\"", comment.char="")
+subject_train <- read.table("~/train/subject_train.txt", quote="\"", comment.char="")
+X_train <- read.table("~/train/X_train.txt", quote="\"", comment.char="")
+y_train <- read.table("~/train/y_train.txt", quote="\"", comment.char="")
 colnames(subject_train) <- c("subject")
 colnames(y_train) <- c("y")
 colnames(X_train) <- features$V2
 train <- cbind(subject_train,X_train,y_train)
 
-subject_test <- read.table("~/subject_test.txt", quote="\"", comment.char="")
-X_test <- read.table("~/X_test.txt", quote="\"", comment.char="")
-y_test <- read.table("~/y_test.txt", quote="\"", comment.char="")
+subject_test <- read.table("~/test/subject_test.txt", quote="\"", comment.char="")
+X_test <- read.table("~/test/X_test.txt", quote="\"", comment.char="")
+y_test <- read.table("~/test/y_test.txt", quote="\"", comment.char="")
 colnames(subject_test) <- c("subject")
 colnames(y_test) <- c("y")
 colnames(X_test) <- features$V2
